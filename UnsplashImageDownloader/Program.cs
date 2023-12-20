4998ec4448b922587b9e6e2d289c8ec5c573b07a
Ajay V MSP <ajay.v@msp-group.co.uk>
2023-12-20 13:58:08 +0530

    Revert "fdgdfg"
    This reverts commit d6d1fb5d74ea9df58d9c1e854239885cb0f01b62.

--- UnsplashImageDownloader/Program.cs
+++ UnsplashImageDownloader/Program.cs
@@ -5,6 +5,9 @@ namespace UnsplashImageDownloader
 {
     static class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
